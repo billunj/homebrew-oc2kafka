588f0aa750f3e6ba2795ae80b5bdaff3c171c8e0
William Urban <william.urban@navigaglobal.com>
2021-01-04 10:59:31 -0500

    version 2021-01-04T155759-b737837

--- oc2kafka.rb
+++ oc2kafka.rb
@@ -5,8 +5,8 @@
 class OC2Kafka < Formula
   desc "UUID Validator"
   homepage "https://bitbucket.org/infomaker/"
-  url  "https://s3-eu-west-1.amazonaws.com/cca-dev-bill-temp/oc2kafka/oc2kafka-2020-12-15T194326-.tar.gz"
-  sha256 "68a4ccb312ef9adff1de978d21dbc5e0073613622c950f6c234ec2cc3bec913b"
+  url  "https://s3-eu-west-1.amazonaws.com/cca-dev-bill-temp/oc2kafka/oc2kafka-2021-01-04T155759-b737837.tar.gz"
+  sha256 "8e2e420d697b842caa6a797d27925308b6b86f0da8f4b56f0dfcf6ab12bc014f"
 
   def install
     bin.install "oc2kafka"
